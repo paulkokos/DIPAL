0df536df6cfffbeae810bd7677154f16d89de426
paulkokos <paulkokos@yahoo.gr>
2020-11-27 21:58:50 +0200

    Structure

---
+++ include/GeometricTransformation.hpp
@@ -0,0 +1,14 @@
+//
+// Created by paulkokos on 11/27/2020.
+//
+
+#ifndef DIPAL_GEOMETRICTRANSFORMATION_HPP
+#define DIPAL_GEOMETRICTRANSFORMATION_HPP
+
+
+class GeometricTransformation {
+
+};
+
+
+#endif //DIPAL_GEOMETRICTRANSFORMATION_HPP
---
+++ include/Noise.h
@@ -0,0 +1,14 @@
+//
+// Created by paulkokos on 11/27/2020.
+//
+
+#ifndef DIPAL_NOISE_H
+#define DIPAL_NOISE_H
+
+
+class Noise {
+
+};
+
+
+#endif //DIPAL_NOISE_H
