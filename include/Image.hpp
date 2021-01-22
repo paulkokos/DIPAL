970467695b16bdda004b6e825237c7a52336c5bd
paulkokos <paulkokos@yahoo.gr>
2021-01-22 23:53:34 +0200

    Initial commit - Image.cpp, Image.hpp

--- include/Image.hpp
+++ include/Image.hpp
@@ -5,9 +5,30 @@
 #ifndef DIPAL_IMAGE_H
 #define DIPAL_IMAGE_H
 
-
+#include <string>
+using std::string;
 class Image {
+private:
+    int height;
+    int width;
+    int colorDepth;
+    string name;
+    string extension;
+public:
+    Image();
+    Image(int height, int width, int colorDepth, string name,string extension);
 
+    int getHeight() const;
+
+    int getWidth() const;
+
+    int getColorDepth() const;
+
+    void setColorDepth(int colorDepth);
+
+    const string &getName() const;
+
+    const string &getExtension() const;
 };
 
 
--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+configure
+make
+sudo make install
