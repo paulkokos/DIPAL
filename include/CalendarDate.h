bd917d66246aca42a7b586a71802e1331a0d080d
Pavlos Kokozidis <paulkokos@yahoo.gr>
2021-03-23 23:51:23 +0200

    Image instantiation

---
+++ include/CalendarDate.h
@@ -0,0 +1,14 @@
+//
+// Created by paulkokos on 3/23/2021.
+//
+
+#ifndef DIPAL_CALENDARDATE_H
+#define DIPAL_CALENDARDATE_H
+
+
+class CalendarDate {
+
+};
+
+
+#endif //DIPAL_CALENDARDATE_H
--- include/Image/Image.hpp
+++ include/Image/Image.hpp
@@ -6,14 +6,31 @@
 #define DIPAL_IMAGE_H
 
 #include <string>
+#include <chrono>
+#include <ctime>
+#include "../CalendarDate.h"
+#include "../CalendarTime.h"
+
 using std::string;
 class Image {
 private:
     int height;
     int width;
     int colorDepth;
+    unsigned int fileSize;
+    int minimumIntensity;
+    int maximumIntensity;
+    string imageClass ;
     string name;
-    string extension;
+    string format;
+    string imageType;
+    struct creationDate {
+        CalendarDate date;
+        CalendarTime time;
+    };
+
+
+
 public:
     Image();
     Image(int height, int width, int colorDepth, string name,string extension);
