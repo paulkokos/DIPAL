21659748f79d9fc644ab5ae4c6165497f6211e82
Pavlos Kokozidis <paulkokos@yahoo.gr>
2021-03-24 02:49:40 +0200

    first imread()

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <windows.h>
+//#include <windows.h>
+#include <vector>
 using namespace std;
 
 int main () {
     ifstream myfile;
-    myfile.open("C:\\search.png");
+    myfile.open("../etc/lena.png",std::ios::binary);
 
     if (myfile.is_open()) {
+        std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(myfile), {});
+        for (int i=0;i< buffer.size();i++) {
+            cout << (int)buffer[i]<< endl;
+        }
         myfile.close();
         cout<< "function success";
     } else {
