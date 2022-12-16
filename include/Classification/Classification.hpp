cefcd8f489621ad5cd4b8fb218589e6b5556da15
paulkokos <paulkokos@yahoo.gr>
2022-12-16 11:31:52 +0200

    Basic transformation classes

---
+++ include/Classification/Classification.hpp
@@ -0,0 +1,14 @@
+//
+// Created by paulk on 12/16/2022.
+//
+
+#ifndef DIPAL_CLASSIFICATION_HPP
+#define DIPAL_CLASSIFICATION_HPP
+
+
+class Classification {
+
+};
+
+
+#endif //DIPAL_CLASSIFICATION_HPP
---
+++ include/Math/Vectors.hpp
@@ -0,0 +1,14 @@
+//
+// Created by paulk on 12/16/2022.
+//
+
+#ifndef DIPAL_VECTORS_HPP
+#define DIPAL_VECTORS_HPP
+
+
+class Vectors {
+
+};
+
+
+#endif //DIPAL_VECTORS_HPP
