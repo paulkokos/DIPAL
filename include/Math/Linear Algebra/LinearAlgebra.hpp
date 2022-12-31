1f2167f81d96b5639797220d827b027a8ac8af67
paulkokos <paulkokos@yahoo.gr>
2022-12-31 23:54:33 +0200

    Math fundamental architecture creation

---
+++ include/Math/Linear Algebra/LinearAlgebra.hpp
@@ -0,0 +1,14 @@
+//
+// Created by paulk on 12/31/2022.
+//
+
+#ifndef DIPAL_LINEARALGEBRA_HPP
+#define DIPAL_LINEARALGEBRA_HPP
+
+
+class LinearAlgebra {
+
+};
+
+
+#endif //DIPAL_LINEARALGEBRA_HPP
