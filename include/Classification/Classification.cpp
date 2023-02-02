ac0c1ec13393b8a6b07c193a7bed222ec8864d55
paulkokos <paulkokos@yahoo.gr>
2023-02-02 14:14:52 +0200

    Color encapsulation

--- include/Classification/Classification.cpp
+++ include/Classification/Classification.cpp
@@ -2,4 +2,10 @@
 // Created by paulk on 12/16/2022.
 //
 
+#include <iostream>
 #include "Classification.hpp"
+namespace DIPAL {
+    Classification::Classification() {
+        std::cout << "This is a classification class call";
+    }
+}
