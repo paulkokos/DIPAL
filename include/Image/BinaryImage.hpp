7238f4880aefa5a093f043cf04a7c461e7964978
paulkokos <paulkokos@yahoo.gr>
2024-02-16 19:52:01 +0200

    Binary Image  and imShow implementation with factory

--- include/Image/BinaryImage.hpp
+++ include/Image/BinaryImage.hpp
@@ -5,17 +5,19 @@
 #ifndef DIPAL_BINARYIMAGE_HPP
 #define DIPAL_BINARYIMAGE_HPP
 #include <iostream>
-#include "Image.hpp"
+#include "ImageFactory.hpp"
+
 namespace DIPAL {
-    class BinaryImage : public Image {
+    class BinaryImage : public ImageFactory {
     public:
         BinaryImage(std::uint64_t *height, std::uint64_t *width, std::uint8_t *colorDepth, std::string *fileName);
 
 
         BinaryImage();
 
-        
-
+        void imShow() const override{
+            std::cout << "Hello from imshow() from Binary\n";
+        }
     };
 }
 
