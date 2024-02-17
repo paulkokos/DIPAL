467157c95d96f10e1f0ab3e31ad2f583e011d4a9
paulkokos <paulkokos@yahoo.gr>
2024-02-17 06:54:37 +0200

    Header setters and getters activated

--- main.cpp
+++ main.cpp
@@ -8,16 +8,19 @@ using namespace DIPAL;
 int main() {
 //    Image *img = new BinaryImage();
 //    img->imShow();
-    ImageFactory* img1 ;
-    ImageFactory* img2 ;
-    ImageFactory* img3 ;
-    ImageFactory* img4 ;
-    ImageFactory* img5 ;
-    img1->build(ImageTypes::IM_Binary);
-    img2->build(ImageTypes::IM_Binary);
-    img3->build(ImageTypes::IM_Binary);
-    img4->build(ImageTypes::IM_Binary);
-    img5->build(ImageTypes::IM_Binary);
+//    ImageFactory* img1 ;
+//    ImageFactory* img2 ;
+//    ImageFactory* img3 ;
+//    ImageFactory* img4 ;
+//    ImageFactory* img5 ;
+//    img1->build(ImageTypes::IM_Binary);
+//    img2->build(ImageTypes::IM_Binary);
+//    img3->build(ImageTypes::IM_Binary);
+//    img4->build(ImageTypes::IM_Binary);
+//    img5->build(ImageTypes::IM_Binary);
+    Image *i = new BinaryImage();
+    i->imShow();
+//    i->helloWorld();
 
     return 0;
 
