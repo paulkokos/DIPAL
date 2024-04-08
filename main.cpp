ad370ac0ff75e3e3c189f05433e3c81b72681422
hakuna-matuti <p.kokozidis@gmail.com>
2024-04-08 17:22:48 +0300

    main.cpp cleanup

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
 #include "include/Image/Image.hpp"
 using namespace DIPAL;
-//Header* Header::headerPtr = nullptr;
 int main() {
-    Image *img;
-    img = new DIPAL::Image();
+    Image *img = new Image();
     delete img;
-
-
-
-//    Image *img = new BinaryImage();
-//    img->imShow();
-//    ImageFactory* img1 ;
-//    ImageFactory* img2 ;
-//    ImageFactory* img3 ;
-//    ImageFactory* img4 ;
-//    ImageFactory* img5 ;
-//    img1->build(ImageTypes::IM_Binary);
-//    img2->build(ImageTypes::IM_Binary);
-//    img3->build(ImageTypes::IM_Binary);
-//    img4->build(ImageTypes::IM_Binary);
-//    img5->build(ImageTypes::IM_Binary);
-//    img5->build(ImageTypes::IM_Grayscale);
     return 0;
 }
