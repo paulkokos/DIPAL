7c5ee304794af3523ab9a2ac1c8cd8fe01b37c4c
Pavlos Kokozidis <paulkokos@yahoo.gr>
2024-10-28 06:26:19 +0200

    working refactor architecture

--- include/Image/Image.hpp
+++ include/Image/Image.hpp
@@ -37,75 +37,6 @@ namespace DIPAL {
     class Image {
         class Header {
         private:
-            unsigned char getSignatureB() const;
-
-            void setSignatureB(unsigned char signatureB);
-
-            unsigned char getSignatureM() const;
-
-            void setSignatureM(unsigned char signatureM);
-
-            uint8_t getReserved1() const;
-
-            void setReserved1(uint8_t reserved1);
-
-            uint8_t getReserved2() const;
-
-            void setReserved2(uint8_t reserved2);
-
-            unsigned int getBitmapInfoHeader() const;
-
-            void setBitmapInfoHeader(unsigned int bitmapInfoHeader);
-
-            unsigned int getWidth() const;
-
-            void setWidth(unsigned int width);
-
-            unsigned int getHeight() const;
-
-            void setHeight(unsigned int height);
-
-            uint8_t getPlanes() const;
-
-            void setPlanes(uint8_t planes);
-
-            unsigned int getOffsetImageStart() const;
-
-            void setOffsetImageStart(unsigned int offsetImageStart);
-
-            uint8_t getBitDepth() const;
-
-            void setBitDepth(uint8_t bitDepth);
-
-            unsigned int getFileSize() const;
-
-            void setFileSize(unsigned int fileSize);
-
-            unsigned int getCompressionType() const;
-
-            void setCompressionType(unsigned int compressionType);
-
-            unsigned int getSizeOfData() const;
-
-            void setSizeOfData(unsigned int sizeOfData);
-
-            unsigned int getHorizontalResolution() const;
-
-            void setHorizontalResolution(unsigned int horizontalResolution);
-
-            unsigned int getVerticalResolution() const;
-
-            void setVerticalResolution(unsigned int verticalResolution);
-
-            unsigned int getNumberOfColors() const;
-
-            void setNumberOfColors(unsigned int numberOfColors);
-
-            unsigned int getNumberOfImportantColors() const;
-
-            void setNumberOfImportantColors(unsigned int numberOfImportantColors);
-
-        private:
             unsigned char signature_B;              //'B' signature (1 byte)
             unsigned char signature_M;              //'M' signature (1 byte)
             uint8_t reserved1;
@@ -126,6 +57,7 @@ namespace DIPAL {
         public:
             Header();
             ~Header();
+            static void helloWorld();
         };
     public:
         Image();
