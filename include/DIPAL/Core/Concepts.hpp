fca5fba3b69168077bc11a5729395eacb2d9fb5a
hakuna-matuti <p.kokozidis@gmail.com>
2025-07-13 01:20:12 +0300

    minnor changes

--- include/DIPAL/Core/Concepts.hpp
+++ include/DIPAL/Core/Concepts.hpp
@@ -2,16 +2,18 @@
 #ifndef DIPAL_CONCEPTS_HPP
 #define DIPAL_CONCEPTS_HPP
 
-#include <memory>
 #include <concepts>
-#include <type_traits>
-#include <span>
+#include <cstdint>
+#include <memory>
 #include <ranges>
+#include <span>
 #include <string>
+#include <type_traits>
 
 namespace DIPAL {
 
 // Concept for types that can be used as pixel values
+
 template <typename T>
 concept Pixel = std::is_arithmetic_v<T> && !std::is_same_v<T, bool>;
 
@@ -25,35 +27,38 @@ concept ImageType = requires(T t, int x, int y) {
 };
 
 // Concept for filter types
-template <typename T>
-concept Filter = ImageType<T> && requires(T t, const T& img) {
-    { t.apply(img) } -> std::convertible_to<std::unique_ptr<T>>;
-    { t.getName() } -> std::convertible_to<std::string>;
+template <typename T, typename ImageT>
+concept Filter = requires(T filter, const T& img) {
+    requires ImageType<ImageT>;
+
+    { filter.apply(img) } -> std::convertible_to<std::unique_ptr<T>>;
+
+    { filter.getName() } -> std::convertible_to<std::string>;
 };
 
 // Concept for processing commands
-template <typename T>
-concept Command = ImageType<T> && requires(T t, const T& img) {
-    { t.execute(img) } -> std::convertible_to<std::unique_ptr<T>>;
-    { t.getName() } -> std::convertible_to<std::string>;
-    { t.isUndoable() } -> std::convertible_to<bool>;
+template <typename T, typename ImageT>
+concept Command = requires(T cmd, const ImageT& img) {
+    requires ImageType<ImageT>;
+    { cmd.execute(img) } -> std::convertible_to<std::unique_ptr<ImageT>>;
+    { cmd.getName() } -> std::convertible_to<std::string>;
+    { cmd.isUndoable() } -> std::convertible_to<bool>;
 };
 
-
-// Concept for range of data
+// Concept for range of data that can be used in image processing
 template <typename T>
-concept DataRange = std::ranges::contiguous_range<T> && 
-                    requires { typename std::ranges::range_value_t<T>; } &&
-                    std::is_trivially_copyable_v<std::ranges::range_value_t<T>>;
+concept DataRange = std::ranges::contiguous_range<T> && requires {
+    typename std::ranges::range_value_t<T>;
+} && std::is_trivially_copyable_v<std::ranges::range_value_t<T>>;
 
-// Concept for transformations
-template <typename T>
-concept Transformation = ImageType<T> && requires(T t, const T& img) {
-    { t.apply(img) } -> std::convertible_to<std::unique_ptr<T>>;
-    { t.getName() } -> std::convertible_to<std::string>;
+// Concept for geometric transformations (rotation, scaling etc)
+template <typename T, typename ImageT>
+concept Transformation = requires(T transform, const ImageT& img) {
+    requires ImageType<ImageT>;
+    { transform.apply(img) } -> std::convertible_to<std::unique_ptr<ImageT>>;
+    { transform.getName() } -> std::convertible_to<std::string>;
 };
 
-} // namespace DIPAL
-
+}  // namespace DIPAL
 
 #endif  // DIPAL_CONCEPTS_HPP
