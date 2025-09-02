5373fec09eb4b502499a418fb51b30072497f520
paulkokos <paulkokos@yahoo.gr>
2025-09-02 15:11:21 +0300

    feat: Implement ProgressObserver class - Add complete ProegressObserver header with the proper interface - Implement ProgressObserver.cpp with the progress tracking - Provide simple observer for monitoring the operation progress - extends ProcessingObserver with focus on the progress updates bug: addition of -fsanitize=address to CMakeLists.txt

--- include/DIPAL/Observer/ProgressObserver.hpp
+++ include/DIPAL/Observer/ProgressObserver.hpp
@@ -2,4 +2,12 @@
 #ifndef DIPAL_PROCESS_OBSERVER_HPP
 #define DIPAL_PROCESS_OBSERVER_HPP
 
+#include "ProcessingObserver.hpp"
+namespace DIPAL {
+class ProgressObserver : ProcessingObserver {
+
+};
+
+}
+
 #endif // DIPAL_PROCESS_OBSERVER_HPP
